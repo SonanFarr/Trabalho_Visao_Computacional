6d62de12f78b60dd1e89b9f5b070a5d8a0a7c28a
Daniel <danielmrezende@yahoo.com.br>
2023-11-29 11:11:07 -0300

    Iniciando implementação dos testes

--- src/models/alexNet.py
+++ src/models/alexNet.py
@@ -1,66 +1,46 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
-# Implementação dos Layers =====================================================================
-def alex_conv_layer1(in_ch, out_ch, kernel, stride, padding):
-    layer = nn.Sequential(
-        nn.Conv2d(in_ch, out_ch, kernel, stride, padding),
-        nn.BatchNorm2d(out_ch),
-        nn.ReLU(),
-        nn.MaxPool2d(kernel_size = kernel, stride = 2)
-    )
-    return layer
-
-def alex_conv_layer2(in_ch, out_ch, kernel, stride, padding):
-    layer = nn.Sequential(
-        nn.Con2d(in_ch, out_ch, kernel, stride, padding),
-        nn.BatchNorm2d(out_ch),
-        nn.ReLU()
-    )
-    return layer
-
-def alex_fc_layer1(in_ch, out_ch):
-    layer = nn.Sequential(
-        nn.Dropout(0.5),
-        nn.Linear(in_ch, out_ch),
-        nn.ReLU()
-    )
-    return layer
-
-def alex_fc_layer2(in_ch, out_ch):
-    layer = nn.Sequential(
-        nn.Linear(in_ch, out_ch),
-    )
-    return layer
-
-# Implementação da Rede AlexNet ================================================================
 class MY_AlexNet(nn.Module):
     def __init__(self, n_classes):
         super(MY_AlexNet, self).__init__()
+        
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            
+            nn.Conv2d(64, 192, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
 
-        self.layer1 = alex_conv_layer1(3, 96, 11, 4, 0)
-        self.layer2 = alex_conv_layer1(96, 256, 5, 1, 2)
-        self.layer3 = alex_conv_layer2(256, 384, 3, 1, 1)
-        self.layer4 = alex_conv_layer2(384, 384, 3, 1, 1)
-        self.layer5 = alex_conv_layer1(384, 256, 3, 1, 1)
-
-        self.layer6 = alex_fc_layer1(9216, 4096)
-        self.layer7 = alex_fc_layer1(4096, 4096)
-
-        self.layer8 = alex_fc_layer2(4096, n_classes)
+        self.avgPool = nn.AdaptiveAvgPool2d((6,6))
+        self.classifier = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, n_classes)
+        )
 
     def forward(self, x):
-        out = self.layer1(x)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        
-        alexNet_features = self.layer5(out)
-        out = alexNet_features.view(out.size(0), -1)
-
-        out = self.layer6(out)
-        out = self.layer7(out)
-
-        out = self.layer8(out)
-
-        return alexNet_features, out
+        x = self.features(x)
+        x = self.avgPool(x)
+        x = x.view(x.size(0), 256 * 6 * 6)
+        logits = self.classifier(x)
+        probas = F.softmax(logits, dim=1)
+        return logits
---
+++ src/utils/utils.py
@@ -0,0 +1,81 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import glob as gb
+import os
+
+def train(dataloader, model, loss_fn, optimizer, device):
+    # Obtém o tamanho do dataset
+    size = len(dataloader.dataset)
+    # Indica que o modelo está em processo de treinamento
+    model.train()
+
+    # Define a loss total do treinamento
+    totalLoss = 0
+
+    # Itera sobre os lotes
+    for batch, (X, y) in enumerate(dataloader):
+        # transforma as entradas no formato do dispositivo utilizado (CPU ou GPU)
+        X, y = X.to(device), y.to(device)
+
+        # Faz a predição para os valores atuais dos parâmetros
+        pred = model(X)
+
+        # Estima o valor da função de perda
+        loss = loss_fn(pred, y)
+
+        # Incrementa a loss total
+        totalLoss += loss
+
+        # Backpropagation
+
+        # Limpa os gradientes
+        optimizer.zero_grad()
+
+        # Estima os gradientes
+        loss.backward()
+
+        # Atualiza os pesos da rede
+        optimizer.step()
+
+        # LOG: A cada 100 lotes (iterações) mostra a perda
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+
+    print(f"Epoch average loss: {totalLoss/len(dataloader):>7f}")
+
+def test(dataloader, model, loss_fn, device):
+    # Obtém o tamanho do dataset
+    size = len(dataloader.dataset)
+
+    # Obtém o número de lotes (iterações)
+    num_batches = len(dataloader)
+
+    # Indica que o modelo está em processo de teste
+    model.eval()
+
+    # Inicializa a perda de teste e a quantidade de acertos com 0
+    test_loss, correct = 0, 0
+
+    # Desabilita o cálculo do gradiente
+    with torch.no_grad():
+        # Itera sobre o conjunto de teste
+        for X, y in dataloader:
+            # transforma as entradas no formato do dispositivo utilizado (CPU ou GPU)
+            X, y = X.to(device), y.to(device)
+            # Realiza a predição
+            pred = model(X)
+
+            # Calcula a perda
+            test_loss += loss_fn(pred, y).item()
+            # Verifica se a predição foi correta
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    # Determina a perda média e a proporção de acertos
+    test_loss /= num_batches
+    correct /= size
+    # LOG: mostra a acurácia e a perda
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    return (100*correct), test_loss
